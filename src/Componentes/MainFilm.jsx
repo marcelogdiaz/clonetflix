e7bf7f1a70338a7c516e56e4f1207102aba49726
mdiaz <mdiaz@armada.mil.ar>
2022-05-19 14:09:25 -0300

    Main Film style

--- src/Componentes/MainFilm.jsx
+++ src/Componentes/MainFilm.jsx
@@ -7,32 +7,26 @@ const MainFilm = ({usuario}) => {
 let mainFilmTag;
 if (usuario.isLoggedIn) {
   console.log("usuario.isLoggedIn MAIN", usuario.isLoggedIn);
-  mainFilmTag = <div className="section_content">
-                <div>
+  mainFilmTag = <div className="mainFilm_content">
                   <img
                         className="d-block w-100"
                         src="https://assets.nflxext.com/ffe/siteui/vlv3/ebff1e0f-5704-423f-b4c6-94914dabe25b/a36a6fb0-5bfb-490f-aa5f-d4b820fff3b5/AR-es-20220509-popsignuptwoweeks-perspective_alpha_website_large.jpg"
                         alt="First slide"
                         opacity= "0.5"
                       />
-                </div>    
                 <div className ="main_film">
                   <h1>RECOMENDACION para {usuario.nombre}</h1>                            
                 </div>
               </div>    
 } else {
   console.log("usuario.isLoggedIn MAIN", usuario.isLoggedIn);
-  mainFilmTag = <div className="section_content">
-                <div>
+  mainFilmTag = <div className="mainFilm_content">
                   <img
                         className="d-block w-100"
                         src="https://assets.nflxext.com/ffe/siteui/vlv3/ebff1e0f-5704-423f-b4c6-94914dabe25b/a36a6fb0-5bfb-490f-aa5f-d4b820fff3b5/AR-es-20220509-popsignuptwoweeks-perspective_alpha_website_large.jpg"
                         alt="First slide"
                         opacity= "0.5"
                       />
-
-                
-                </div>    
                 <div className ="main_film">
                   <h1>Películas y series ilimitadas y mucho más.</h1>          
                   <h4>{usuario.nombre} : Disfruta donde quieras. Cancela cuando quieras.</h4>
