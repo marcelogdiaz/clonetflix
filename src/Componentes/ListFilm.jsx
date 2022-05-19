400b0291248848f6c237e4a3bca2ec35e1524907
mdiaz <mdiaz@armada.mil.ar>
2022-05-19 12:23:46 -0300

    Carrousel

--- src/Componentes/ListFilm.jsx
+++ src/Componentes/ListFilm.jsx
@@ -1,9 +1,11 @@
 
 import React from "react";
 import { useEffect, useState } from "react";
-import { Badge, Button, Card, CardGroup, CardImg } from "react-bootstrap";
+import { Card, CardImg } from "react-bootstrap";
 import styles from './ListFilm.css';
 import axios from "axios";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const ListFilm = ({usuario}) => {
 
@@ -59,35 +61,21 @@ const ListFilm = ({usuario}) => {
         ):
         (                 
           <>
-            <section className="section_content">
+            <section className="listfilm_content">
             <h3>Populares en Netflix</h3>
-            {/* <CardGroup> */}
+            <Swiper
+                    slidesPerView={movies.length/2} >
             {             
                movies.map(movie => {
                   let path= "https://image.tmdb.org/t/p/w200/"+ movie.poster_path;
-                  return (
-                  <Card                    
-                    key={movie.id}
-                    // text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-                    // style={{ width: '18rem', display: 'inline', float: 'left' }}
-                    className="col-sm-2">                    
-                    <CardImg variant="top" src={path} />
-                    {/* <Card.Body>
-                      <Card.Text>
-                         <div>{(movie.overview).substring(0,100)+"..."}</div>
-                         <div>
-                         <Badge 
-                          variant="secondary"
-                          pill="true">
-                            {movie.vote_average}</Badge>
-                            </div>
-                      </Card.Text>
-                    </Card.Body> */}
-                    <Card.Header>{movie.title}</Card.Header>
-                  </Card>                  
-               )})
+                  return (                    
+                      <SwiperSlide>
+                          <img variant="top" src={path} />
+                          <div className="swipertitle">{movie.title}</div>
+                      </SwiperSlide>
+               )})               
              }   
-            {/* </CardGroup> */}
+             </Swiper>
             </section>
           </>
         )
